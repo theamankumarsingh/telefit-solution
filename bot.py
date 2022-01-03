2e1d4472889e425427983bd172982ec25406b510
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2022-01-03 18:07:44 +0530

    feat: add commands

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 import telebot
+import requests
 
+#NUTRITIONIX_API_KEY = os.environ['NUTRITIONIX_API_KEY']
 API_KEY = os.environ['API_KEY']
 bot = telebot.TeleBot(API_KEY)
 botRunning=False
@@ -17,8 +19,30 @@ def goodbye(message):
     botRunning=False
     bot.reply_to(message, 'Bye!')
 
+@bot.message_handler(func=lambda message: botRunning, commands=['nutrition'])
+def getNutrition(message):
+    bot.reply_to(message, 'Getting nutrition info...')
+    bot.send_message(message.chat.id, message.text[11:])
+    bot.send_message(message.chat.id, 'Nutrition info:')
+    bot.send_message(message.chat.id, 'Calories:')
+    bot.send_message(message.chat.id, 'Fat:')
+    bot.send_message(message.chat.id, 'Carbohydrates:')
+    bot.send_message(message.chat.id, 'Protein:')
+
+@bot.message_handler(func=lambda message: botRunning, commands=['exercise'])
+def getCaloriesBurn(message):
+    bot.reply_to(message, 'Estimating calories burned...')
+    bot.send_message(message.chat.id, message.text[9:])
+    bot.send_message(message.chat.id, 'Calories burned:')
+
+@bot.message_handler(func=lambda message: botRunning, commands=['report'])
+def getCaloriesBurn(message):
+    bot.reply_to(message, 'Generating report...')
+    bot.send_message(message.chat.id, message.text[7:])
+    bot.send_message(message.chat.id, 'CSV file:')
+
 @bot.message_handler(func=lambda message: botRunning)
 def default(message):
     bot.reply_to(message, 'I did not understand :(')
 
-bot.polling()
+bot.infinity_polling()
